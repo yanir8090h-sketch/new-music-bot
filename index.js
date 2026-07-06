e9c12576992c00f7ee0a10ccf5f4d9cb8825521a
yanir8090h-sketch <yanir8090h@gmail.com>
2026-07-06 18:26:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -153,25 +153,28 @@ client.on('interactionCreate', async (interaction) => {
 
             
                    // חיפוש השיר ביוטיוב לפי השם שהמשתמש הזין
-        const play = require('play-dl');
-        let songName = choiceName; // משתנה שם השיר מהמודאל שלך
-        
+               const play = require('play-dl');
+
+        // חיפוש השיר ביוטיוב
         let yt_info = await play.search(songName, { limit: 1 });
         if (!yt_info || yt_info.length === 0) {
             return await interaction.editReply({ content: '❌ לא מצאתי שיר בשם הזה!', ephemeral: true });
         }
 
+        // יצירת זרם האודיו מהסרטון שנמצא
         let stream = await play.stream(yt_info[0].url, { discordPlayerCompatibility: true });
         const resource = createAudioResource(stream.stream, {
             inputType: stream.type
         });
 
-
-
         player.play(resource);
         connection.subscribe(player);
 
-      await interaction.editReply({ content: `🎶 הבוט מזרים כעת בהצלחה בחדר הקול "${choiceName}" מותאם בבטחה ופותח את ערוץ השמע על ידי: ${interaction.user}`, ephemeral: true });
+        await interaction.editReply({ content: `🎶 הבוט מזרים כעת בהצלחה בחדר הקול את השיר: **${yt_info[0].title}** מותאם בבטחה ופותח את ערוץ השמע על ידי: ${interaction.user}`, ephemeral: true });
+
+
+
+       
 
 
        
